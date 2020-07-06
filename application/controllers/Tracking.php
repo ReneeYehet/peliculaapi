73ef6cc1403734317467e7bb400379192ad0ce1e
ReneeYehet <44626852+ReneeYehet@users.noreply.github.com>
2020-07-06 10:51:59 -0700

    Create Tracking.php

---
+++ application/controllers/Tracking.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+
+class Tracking extends CI_Controller {
+
+    public function callAPI(){
+        $url = 'https://api.dhl.com/dgff/transportation/shipment-tracking';
+        $tracking_number = 'SCLA00055';
+
+        $request_url = $url . '/' .$tracking_number;
+                    
+        /* Init cURL resource */
+        $curl = curl_init($request_url);
+            
+        /* Array Parameter Data 
+        $data = ['housebill: "'.$tracking_number.'"'];
+        */
+
+        /*curl_setopt($curl, CURLOPT_URL, 'https://api.dhl.com/dgff/transportation/shipment-tracking');*/
+        /*curl_setopt($curl, CURLOPT_POST, true);*/
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'accept: application/json',
+            'DHL-API-Key: oWzKTO2B7znEMmtaY9vr7t3pvv5E7SO9'
+        ]);
+            
+        /* pass encoded JSON string to the POST fields 
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);*/
+        
+        /* set return type json 
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);*/
+            
+        /* execute request */
+        $result = curl_exec($curl);
+        
+
+        $err = curl_error($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $response = json_decode($result, true);
+            print_r($response);
+            curl_close($curl);
+        }
+    }
+}
+    
+
